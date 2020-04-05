4a62f5b791561a726b689d90d6883c57fd8a5830
ahmed emad <ahmed.emad2051@gmail.com>
2020-04-05 14:38:16 +0200

    rm profile

--- backend.tf
+++ backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket = "terraform-demo01"
     key    = "state/dev/terraform.tfstate"
     region = "us-west-1"
-    shared_credentials_file = "/home/onepiece/.aws/credentials"
-    profile = "default"
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
   region = "${var.provider_region}"  
-  shared_credentials_file = "/home/onepiece/.aws/credentials"
-  profile = "${var.provider_profile}"
 }
